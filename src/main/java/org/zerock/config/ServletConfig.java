138da7c02f1b8b6f9fcb38bc72a73a647bc989a8
황정현 <hwangjeonghyeon@hwangjeonghyeon-ui-MacBookPro.local>
2020-12-04 19:01:46 +0900

    Spring MVC 구조

--- src/main/java/org/zerock/config/ServletConfig.java
+++ src/main/java/org/zerock/config/ServletConfig.java
@@ -1,11 +1,13 @@
 package org.zerock.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+@ComponentScan(basePackages = {"org.zerock.controller"})
 public class ServletConfig  implements WebMvcConfigurer {
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry)  {
--- src/main/java/org/zerock/controller/SampleController.java
+++ src/main/java/org/zerock/controller/SampleController.java
@@ -4,15 +4,51 @@ package org.zerock.controller;
 import lombok.extern.log4j.Log4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.zerock.domain.SampleDTO;
+
+import java.util.ArrayList;
 
 @Controller
 @Log4j
+@RequestMapping("/sample/*")
 public class SampleController {
 
-    @GetMapping("/doA")
-    public void doA(){
-        System.out.println("굿굿");
-        log.info("#### $$$$$ @@@2");
-        log.info("doA ... 실행");
+    @RequestMapping("")
+    public void basic(){
+        log.debug("#### $$$$$ @@@");
+        log.info("bbasic.............");
     }
+
+    @RequestMapping(value="/basic", method = {RequestMethod.GET,RequestMethod.POST})
+    public void basicGet(){
+        log.info("basic get...");
+    }
+
+    @GetMapping("/basicOnlyGet")
+    public void basicGet2(){
+        log.info("basic get only .....");
+    }
+
+    @GetMapping("/ex01")
+    public String ex01(SampleDTO dto){
+        log.info("#" + dto);
+        return "ex01";
+    }
+
+    @GetMapping("/ex02")
+    public String ex02(@RequestParam("name") String name, @RequestParam("age") int age){
+        log.info(name);
+        log.info(age);
+        return "ex02";
+    }
+
+    @GetMapping("/ex02List")
+    public String ex02List(@RequestParam("ids") String[] ids){
+        log.info(ids.toString());
+        return "ex02List";
+    }
+
 }
